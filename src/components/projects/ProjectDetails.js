7ae410aaa65441d075c0a4de9d8cdd1df1812810
SamuelAl <s.alarco@outlook.com>
2020-08-10 17:34:00 +0100

    Implemented Signin and Signup template

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 function ProjectDetails(props) {
+    const id = props.match.params.id
+
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Project Title</span>
+                    <span className="card-title">Project Title - {id}</span>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti blanditiis quae optio, vel a excepturi non sint nemo aspernatur alias nesciunt dignissimos quibusdam, accusamus repudiandae atque porro earum? Facilis?</p>
                 </div>
                 <div className="card-action grey lighten-4 grey-text">
