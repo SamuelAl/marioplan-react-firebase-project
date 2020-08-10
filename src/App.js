cc55fbedfdee99dc48875d57c66128a12bff49d1
SamuelAl <s.alarco@outlook.com>
2020-08-10 16:05:00 +0100

    Created navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter} from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>Marioplan</h1>
+        <Navbar />
       </div>
     </BrowserRouter>
   );
