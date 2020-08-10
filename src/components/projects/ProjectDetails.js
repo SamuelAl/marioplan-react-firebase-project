c7684774ade4273a6b79246b84e9fee328983a2e
SamuelAl <s.alarco@outlook.com>
2020-08-10 16:55:19 +0100

    Layout of project dashboard

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProjectDetails(props) {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Project Title</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti blanditiis quae optio, vel a excepturi non sint nemo aspernatur alias nesciunt dignissimos quibusdam, accusamus repudiandae atque porro earum? Facilis?</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by the Net Ninja</div>
+                    <div>3/10/2020</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
